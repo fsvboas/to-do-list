a6526870e08ab2e59b392c7b32716ac4f0875851
fsvboas <fsvboas.dev@gmail.com>
2025-02-10 18:05:39 -0300

    feat: add show component

---
+++ src/app/components/utils/show.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface ShowProps {
+  when?: unknown
+  fallback?: React.ReactNode
+  children?: React.ReactNode | React.ReactNode[]
+}
+export const Show = (props: ShowProps) => {
+  return <>{props.when ? props.children : props.fallback}</>
+}
